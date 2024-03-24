c1a5f85f915a037705bf8a432f2b866f24206781
icr4m <jaber.icham@gmail.com>
2024-03-24 13:50:34 +0100

    1

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: ijaber <ijaber@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 19:49:10 by ijaber            #+#    #+#             */
-/*   Updated: 2024/03/24 03:12:19 by ijaber           ###   ########.fr       */
+/*   Updated: 2024/03/24 13:46:47 by ijaber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,17 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	char	*tmp_dst;
 	char	*tmp_src;
-	size_t	i;
 
 	if (!dst && !src)
 		return (NULL);
 	tmp_dst = (char *)dst;
 	tmp_src = (char *)src;
-	i = 0;
 	if (tmp_dst > tmp_src)
 	{
 		while (len-- > 0)
-		{
 			tmp_dst[len] = tmp_src[len];
-		}
 	}
 	else
-	{
-		while (i++ < len)
-		{
-			tmp_dst[i] = tmp_src[i];
-		}
-	}
+		ft_memcpy(dst, src, len);
 	return (dst);
 }
