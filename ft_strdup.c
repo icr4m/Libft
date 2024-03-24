268fc7bc51f7b5027181f70de3289e73fa4f3880
icr4m <jaber.icham@gmail.com>
2024-03-24 17:33:37 +0100

    update

---
+++ ft_strdup.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ijaber <ijaber@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/24 17:05:27 by ijaber            #+#    #+#             */
+/*   Updated: 2024/03/24 17:33:28 by ijaber           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s1)
+{
+	char *str;
+	size_t i;
+
+	str = (char *)malloc(ft_strlen(s1) * sizeof(char) + 1);
+	if (!str)
+		return (NULL);
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
---
+++ ft_strnstr.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ijaber <ijaber@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/24 15:18:01 by ijaber            #+#    #+#             */
+/*   Updated: 2024/03/24 17:03:31 by ijaber           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	if (!needle)
+		return ((char *)haystack);
+	while (i < len && haystack[i] != '\0')
+	{
+		j = 0;
+		if (haystack[i] == needle[j])
+		{
+			while (haystack[i + j] == needle[j] && needle[j] != '\0')
+			{
+				j++;
+				if (needle[j] == '\0')
+					return ((char *)haystack + i);
+			}
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+/*
+int	main(int ac, char **av)
+{
+	if (ac == 3)
+		printf("test : %s", ft_strnstr(av[1], av[2], 20));
+}
+*/
