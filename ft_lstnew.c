76da6b1f5bee46d5fa494b2b1c202aa314d65334
icr4m <jaber.icham@gmail.com>
2024-05-05 19:21:43 +0200

    update

---
+++ ft_lstnew.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ijaber <ijaber@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/05 18:46:37 by ijaber            #+#    #+#             */
+/*   Updated: 2024/05/05 19:21:35 by ijaber           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list *new;
+
+	new = malloc(sizeof(t_list));
+	if (!new)
+		return (NULL);
+	new->content = content;
+	new->next = NULL;
+	return (new);
+}
