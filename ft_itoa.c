cfde9cf95245973579c7eb7698fc3bc0811068e3
Icham Jaber <ijaber@paul-f4Br7s4.clusters.42paris.fr>
2024-05-14 14:57:03 +0200

    rectif-itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,23 +6,26 @@
 /*   By: ijaber <ijaber@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 22:55:14 by ijaber            #+#    #+#             */
-/*   Updated: 2024/03/28 00:17:19 by ijaber           ###   ########.fr       */
+/*   Updated: 2024/05/14 14:54:47 by ijaber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	nb_len(long nb)
+// Donne la valeur absolue
+unsigned int	get_abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	return (n);
+}
+
+size_t	nb_len(int nb)
 {
 	int	len;
 
 	len = 0;
-	if (nb < 0)
-	{
-		nb *= -1;
-		len++;
-	}
-	while (nb > 0)
+	while (nb != 0)
 	{
 		nb /= 10;
 		len++;
@@ -30,38 +33,35 @@ int	nb_len(long nb)
 	return (len);
 }
 
-char	*ft_itoa(int nb)
+void	fill_number(char *src, int nb, int len)
 {
-	char	*result;
-	long	n;
-	int		len;
+	unsigned int	i;
+	unsigned int	n_abs;
 
-	n = nb;
-	len = nb_len(n);
-	if (!(result = (char *)malloc(len + 1)))
-		return (0);
-	result[len--] = '\0';
-	if (n < 0)
+	i = 0;
+	n_abs = get_abs(nb);
+	while (i < len)
 	{
-		result[0] = '-';
-		n *= -1;
+		src[len - i - 1] = (n_abs % 10) + '0';
+		n_abs /= 10;
+		i++;
 	}
-	if (n == 0)
-	{
-		result[0] = '0';
-		return (result);
-	}
-	while (n > 0)
-	{
-		result[len] = (n % 10) + '0';
-		n /= 10;
-		len--;
-	}
-	return (result);
+	src[len] = '\0';
 }
-/*
-int	main(void)
+
+char	*ft_itoa(int n)
 {
-	printf("la string :%s\n", ft_itoa(-2147483648));
+	char	*new;
+	int		len;
+	bool	is_neg;
+
+	is_neg = (n < 0);
+	len = nb_len(n);
+	new = (char *)malloc((len + is_neg) * sizeof(char) + 1);
+	if (!new)
+		return (0);
+	if (is_neg)
+		new[0] = '-';
+	fill_number(new + is_neg, n, len);
+	return (new);
 }
-*/
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: ijaber <ijaber@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/24 15:18:01 by ijaber            #+#    #+#             */
-/*   Updated: 2024/05/14 11:36:07 by ijaber           ###   ########.fr       */
+/*   Updated: 2024/05/14 13:42:51 by ijaber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,8 +37,10 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	return (NULL);
 }
 
+/*
 int	main(int ac, char **av)
 {
 	if (ac == 3)
 		printf("test : %s", ft_strnstr(av[1], av[2], 20));
 }
+*/
