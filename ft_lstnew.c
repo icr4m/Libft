f4afdd5c3d2158755d837f0c34dda75590dee99e
Icham Jaber <ijaber@paul-f4Br7s4.clusters.42paris.fr>
2024-05-16 15:36:37 +0200

    update

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: ijaber <ijaber@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/05 18:46:37 by ijaber            #+#    #+#             */
-/*   Updated: 2024/05/16 14:01:40 by ijaber           ###   ########.fr       */
+/*   Updated: 2024/05/16 15:11:11 by ijaber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,15 @@ t_list	*ft_lstnew(void *content)
 	if (!new)
 		return (NULL);
 	new->content = content;
-	new->content = NULL;
+	new->next = NULL;
 	return (new);
+}
+
+int	main(void)
+{
+	t_list	*test;
+
+	test = ft_lstnew("Oui");
+	printf("AND %s\n", test->content);
+	return (0);
 }
