993a0f2041849d0ec19ee909ce7656b8eb63b759
Icham Jaber <ijaber@paul-f4Br7s4.clusters.42paris.fr>
2024-05-16 16:52:18 +0200

    strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: ijaber <ijaber@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/24 15:18:01 by ijaber            #+#    #+#             */
-/*   Updated: 2024/05/14 13:42:51 by ijaber           ###   ########.fr       */
+/*   Updated: 2024/05/16 16:52:06 by ijaber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,19 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t	i;
 	size_t	j;
 
-	if (!needle)
+	if (needle[0] == '\0')
 		return ((char *)haystack);
+	// if (!needle)
+	// return ((char *)haystack);
 	i = 0;
 	while (i < len && haystack[i] != '\0')
 	{
 		j = 0;
-		if (haystack[i] == needle[j])
-		{
-			while (haystack[i + j] == needle[j] && needle[j] != '\0')
-			{
-				j++;
-				if (needle[j] == '\0')
-					return ((char *)haystack + i);
-			}
-		}
+		while (needle[j] != '\0' && haystack[i + j] != '\0'
+			&& needle[j] == haystack[i + j] && i + j < len)
+			++j;
+		if (needle[j] == '\0')
+			return ((char *)haystack + i);
 		i++;
 	}
 	return (NULL);
@@ -41,6 +39,6 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 int	main(int ac, char **av)
 {
 	if (ac == 3)
-		printf("test : %s", ft_strnstr(av[1], av[2], 20));
+		printf("test : %s", ft_strnstr(av[1], av[2], -1));
 }
 */
