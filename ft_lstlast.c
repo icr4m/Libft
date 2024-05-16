a081e74ce25dd6bb14fac1e729e9edfff1fbda0f
Icham Jaber <ijaber@paul-f4Br7s4.clusters.42paris.fr>
2024-05-16 18:23:55 +0200

    fix_list

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: ijaber <ijaber@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 23:15:50 by ijaber            #+#    #+#             */
-/*   Updated: 2024/05/09 23:16:21 by ijaber           ###   ########.fr       */
+/*   Updated: 2024/05/16 18:15:39 by ijaber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 t_list	*ft_lstlast(t_list *lst)
 {
-	t_list *tmp;
+	t_list *last;
 
 	if (!lst)
 		return (NULL);
-	tmp = lst;
-	while (tmp->next)
-		tmp = tmp->next;
-	return (tmp);
+	last = lst;
+	while (lst->next)
+		last = lst->next;
+	return (last);
 }
--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: ijaber <ijaber@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/05 18:46:37 by ijaber            #+#    #+#             */
-/*   Updated: 2024/05/16 15:11:11 by ijaber           ###   ########.fr       */
+/*   Updated: 2024/05/16 17:57:48 by ijaber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ t_list	*ft_lstnew(void *content)
 	new->next = NULL;
 	return (new);
 }
-
+/*
 int	main(void)
 {
 	t_list	*test;
@@ -32,3 +32,4 @@ int	main(void)
 	printf("AND %s\n", test->content);
 	return (0);
 }
+*/
